d01cabc9a5c30eeb5ea88a64141ce0709df1d74e
Patrick Easton <patrickeaston151@gmail.com>
2021-01-20 19:44:18 +0000

    Changed how summarisation is done by using nltk instead of gensim. Summarise method returns 5 sentences in total.

--- macaw/core/retrieval/whoosh.py
+++ macaw/core/retrieval/whoosh.py
@@ -8,10 +8,14 @@ from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
 from whoosh.qparser import OrGroup
 
-from gensim.summarization import summarize
-
 from macaw.core.retrieval.doc import Document
 from macaw.core.retrieval.search_engine import Retrieval
+
+import re
+import heapq
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
 class Whoosh(Retrieval):
@@ -26,15 +30,51 @@ class Whoosh(Retrieval):
             q = QueryParser("content", schema=self.index.schema, group=OrGroup).parse(query)
             results = s.search(q, limit=self.results_requested)
             for r in results:
-                summary = summarize(r["content"], word_count=50)
+                summary = self.summarise(r["content"])
                 docs.append(Document(r["title"], r["url"], summary, r.score))
         return docs
+
+    def summarise(self, doc_text):
+        doc_text = re.sub(r'\[[0-9]*\]', ' ', doc_text)
+        doc_text = re.sub(r'\s+', ' ', doc_text)
+
+        formatted_doc_text = re.sub('[^a-zA-Z]', ' ', doc_text)
+        formatted_doc_text = re.sub(r'\s+', ' ', formatted_doc_text)
+
+        sentence_list = nltk.sent_tokenize(doc_text)
+        stopwords = nltk.corpus.stopwords.words('english')
+
+        word_frequencies = {}
+        for word in nltk.word_tokenize(formatted_doc_text):
+            if word not in stopwords:
+                if word not in word_frequencies.keys():
+                    word_frequencies[word] = 1
+                else:
+                    word_frequencies[word] += 1
+
+        maximum_frequency = max(word_frequencies.values())
+
+        for word in word_frequencies.keys():
+            word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
+
+        sentence_scores = {}
+        for sent in sentence_list:
+            for word in nltk.word_tokenize(sent.lower()):
+                if word in word_frequencies.keys():
+                    if len(sent.split(' ')) < 30:
+                        if sent not in sentence_scores.keys():
+                            sentence_scores[sent] = word_frequencies[word]
+                        else:
+                            sentence_scores[sent] += word_frequencies[word]
+
+        summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
+        summary = ' '.join(summary_sentences)
+        return summary
 
     def get_doc_from_index(self, doc_id):
         doc = []
         with self.index.searcher() as s:
             q = QueryParser("title", schema=self.index.schema).parse(doc_id)
             r = s.search(q, limit=1)
-            summary = summarize(r["content"], word_count=50)
-            doc.append(Document(r["title"], r["url"], summary, r.score))
+            doc.append(Document(r["title"], r["url"], r["content"], r.score))
         return doc

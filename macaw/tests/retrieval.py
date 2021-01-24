41f6df6d5490465fbdbc317d6637d444576f5c99
Patrick Easton <patrickeaston151@gmail.com>
2021-01-24 14:10:53 +0000

    Added unit tests for retrieval and summarisation. Not great but can be improved.

---
+++ macaw/tests/retrieval.py
@@ -0,0 +1,117 @@
+import unittest
+from macaw.core import retrieval
+from macaw.util.logging import Logger
+
+
+class RetrievalTestCases(unittest.TestCase):
+    retrieval_params = {'search_engine': 'whoosh',
+                        'query_generation': 'simple',
+                        'col_index': '/home/patrick-easton/Documents/CSA_Project_Patrick_Easton_Macaw/whoosh/indexdir',
+                        'results_requested': 3,
+                        'logger': Logger({})}
+
+    retrieval_params['logger'].info(retrieval_params)
+
+    def test_retrieval_manhattan_project_1(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("The Manhattan Project?")
+        self.assertEqual(doc[0].id, '2')
+
+    def test_retrieval_manhattan_project_2(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Lets talk about the Manhattan Project?")
+        self.assertEqual(doc[0].id, '2')
+
+    def test_retrieval_manhattan_project_3(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Tell me about the Manhattan Project?")
+        self.assertEqual(doc[0].id, '2')
+
+    def test_retrieval_manhattan_project_4(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("What is the Manhattan Project?")
+        self.assertEqual(doc[0].id, '2')
+
+    def test_retrieval_manhattan_project_5(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("What do you know about the Manhattan Project?")
+        self.assertEqual(doc[0].id, '2')
+
+    def test_retrieval_restorative_justice_1(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Restorative Justice?")
+        self.assertEqual(doc[0].id, '14')
+
+    def test_retrieval_restorative_justice_2(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Lets talk about Restorative Justice?")
+        self.assertEqual(doc[0].id, '14')
+
+    def test_retrieval_restorative_justice_3(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Tell me about Restorative Justice?")
+        self.assertEqual(doc[0].id, '14')
+
+    def test_retrieval_restorative_justice_4(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("What is Restorative Justice?")
+        self.assertEqual(doc[0].id, '14')
+
+    def test_retrieval_restorative_justice_5(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("What do you know about Restorative Justice?")
+        self.assertEqual(doc[0].id, '14')
+
+    def test_retrieval_immigration_1(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Chinese Immigration?")
+        self.assertEqual(doc[0].id, '31')
+
+    def test_retrieval_immigration_2(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Lets talk about Chinese Immigration?")
+        self.assertEqual(doc[0].id, '31')
+
+    def test_retrieval_immigration_3(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Tell me about Chinese Immigration?")
+        self.assertEqual(doc[0].id, '31')
+
+    def test_retrieval_immigration_4(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("How big is Chinese Immigration to America?")
+        self.assertEqual(doc[0].id, '31')
+
+    def test_retrieval_immigration_5(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("What do you know about Chinese Immigration?")
+        self.assertEqual(doc[0].id, '31')
+
+    def test_retrieval_medical_tourism_1(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Medical Tourism in Costa Rica?")
+        self.assertEqual(doc[0].id, '39')
+
+    def test_retrieval_medical_tourism_2(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Lets talk about Medical Tourism in Costa Rica?")
+        self.assertEqual(doc[0].id, '39')
+
+    def test_retrieval_medical_tourism_3(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("Tell me about Medical Tourism in Costa Rica?")
+        self.assertEqual(doc[0].id, '39')
+
+    def test_retrieval_medical_tourism_4(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("How prevalent is medical tourism in Costa Rica?")
+        self.assertEqual(doc[0].id, '39')
+
+    def test_retrieval_medical_tourism_5(self):
+        whoosh = retrieval.get_retrieval_model(params=self.retrieval_params)
+        doc = whoosh.retrieve("What do you know about Medical Tourism in Costa Rica?")
+        self.assertEqual(doc[0].id, '39')
+
+
+if __name__ == '__main__':
+    unittest.main()
